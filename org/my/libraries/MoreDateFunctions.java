ce161bb47ca8f0a687d6fafd4fed68d5ef36e8d2
Cory <coryma86@gmail.com>
2015-03-23 08:54:24 -0400

    Modified MoreDateFunctions

--- org/my/libraries/MoreDateFunctions.java
+++ org/my/libraries/MoreDateFunctions.java
@@ -8,12 +8,13 @@ import java.util.Locale;
 public final class MoreDateFunctions
 {
 	private final static SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMdd", Locale.US);
+    private final static SimpleDateFormat slashDateFormat = new SimpleDateFormat("MM/dd/yy", Locale.US);
 	
-	public static double timeDiffInDays(Date subtrFromDate, Date subtrWithDate)
-	{
-		return (timeDiffInHours(subtrFromDate, subtrWithDate) / 24);
-	}
-	
+    public static double timeDiffInDays(Date subtrFromDate, Date subtrWithDate)
+    {
+        return (timeDiffInHours(subtrFromDate, subtrWithDate) / 24);
+    }
+
 	public static double timeDiffInHours(Date subtrFromDate, Date subtrWithDate)
 	{
 		return (timeDiffInMinutes(subtrFromDate, subtrWithDate) / 60);
@@ -28,14 +29,19 @@ public final class MoreDateFunctions
 	{
 		return ((double)(subtrFromDate.getTime() - subtrWithDate.getTime()) / 1000);
 	}
-	
-	public static String getTodayYYMMDD()
-	{
-		return dateFormat.format(new Date());
-	}
-	
-	public static Date getDateFromYYMMDD(String dateString) throws ParseException
-	{
-		return dateFormat.parse(dateString);
-	}
+
+    public static String getTodayYYMMDD()
+    {
+        return (dateFormat.format(new Date()));
+    }
+
+    public static String getTodaySlashMMDDYY()
+    {
+        return (slashDateFormat.format(new Date()));
+    }
+
+    public static Date getDateFromYYMMDD(String dateString) throws ParseException
+    {
+        return dateFormat.parse(dateString);
+    }
 }
