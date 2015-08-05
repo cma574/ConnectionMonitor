f71d10dafe7ac1a6f09f7a0686337012b220811e
Cory <coryma86@gmail.com>
2015-08-05 12:09:06 -0400

    Refactored code

---
+++ CheckNeedEmailResponse.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+/**
+ * An object for holding information for whether an email needs to be sent and what information to query on.
+ * @author Cory Ma
+ */
+public class CheckNeedEmailResponse
+{
+	private boolean shouldSend;
+	private Date startDateQuery, endDateQuery;
+	
+	/**
+	 * Constructor. Sets shouldSend to false by default.
+	 */
+	public CheckNeedEmailResponse()
+	{
+		shouldSend = false;
+	}
+	
+	/**
+	 * Constructor. Sets shouldSend to true.
+	 * @param startDate     Time the first site first became unreachable.
+	 * @param endDate       Time the last site unreachable became reachable again
+	 */
+	public CheckNeedEmailResponse(Date startDate, Date endDate)
+	{
+		shouldSend = true;
+		startDateQuery = startDate;
+		endDateQuery = endDate;
+	}
+	
+	/**
+	 * Gets whether a message should be sent or not.
+	 * @return true if a message should be sent, false if not
+	 */
+	public boolean getShouldSend()
+	{
+		return shouldSend;
+	}
+	
+	/**
+	 * Gets the time for the query to start.
+	 * @return The start Date for querying
+	 */
+	public Date getStartDateQuery()
+	{
+		return startDateQuery;
+	}
+	
+	/**
+	 * Gets the time for the query to end.
+	 * @return The end Date for querying
+	 */
+	public Date getEndDateQuery()
+	{
+		return endDateQuery;
+	}
+}
---
+++ org/my/libraries/IOUtilities.java
@@ -0,0 +1,70 @@
+package org.my.libraries;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Library of commonly used IO related functions.
+ * @author Cory Ma
+ */
+public class IOUtilities
+{
+	/**
+	 * Reads a line of text from the console input.
+	 * @return The input received from the console
+	 * @throws IOException
+	 */
+	public static String readConsoleLine() throws IOException
+	{
+		String input = "";
+		Console console = System.console();
+	    if(console == null)
+	    {
+	    	input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+	    }
+	    else
+	    {
+	    	input = console.readLine();
+	    }
+	    
+		return input;
+	}
+	
+	/**
+	 * Reads a line of text from the console input with echoing disabled unless run from an IDE.
+	 * @return The input received from the console
+	 * @throws IOException
+	 */
+	public static String readPassword() throws IOException
+	{
+		String input = "";
+		Console console = System.console();
+	    if(console == null)
+	    {
+	    	input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+	    }
+	    else
+	    {
+	    	input = new String(console.readPassword());
+	    }
+	    
+		return input;
+	}
+	
+	/**
+	 * Calls close() on a Closeable object and ignores any IOExceptions thrown.
+	 * @param closeable     Object to close
+	 */
+	public static void closeCloseable(Closeable closeable)
+	{
+		try
+		{
+			closeable.close();
+		}
+		catch(IOException e)
+		{}
+	}
+}
